5cd1a92d2d5cefe520df1cca55df5a87160c7204
oswalkarishma26 <107704168+oswalkarishma26@users.noreply.github.com>
2022-07-03 00:15:47 +0530

    Update TechDept.java

--- OOPsAssignment1Solution/src/com/greatlearning/departments/TechDept.java
+++ OOPsAssignment1Solution/src/com/greatlearning/departments/TechDept.java
@@ -3,23 +3,19 @@ package com.greatlearning.departments;
 public class TechDept extends SuperDept{
 	
 	public String departmentName() {
-		System.out.println("  Welcome to Tech Department ");
-		return " Tech Department ";
+		return "  Welcome to Tech Department ";
 	}
 	
 	public String getTodaysWork() {
-		System.out.println(" Complete coding of Module 1");
-		return " Complete coding of module 1";
+		return " Complete coding of Module 1";
 	}
 	
 	public String getWorkDeadline() {
-		System.out.println(" Complete by EOD");
 		return " Complete by EOD";
 	}
 	
 	public String getTechStackInformation() {
-		System.out.println(" Core Java");
-		return "Core Java";
+		return " Core Java";
 	}
 
 }
