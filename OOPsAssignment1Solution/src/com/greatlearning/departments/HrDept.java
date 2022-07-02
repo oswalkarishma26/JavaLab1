3e214c4d297cc4594da3ccd106f95cea87c2db5a
oswalkarishma26 <107704168+oswalkarishma26@users.noreply.github.com>
2022-07-03 00:13:31 +0530

    Update HrDept.java

--- OOPsAssignment1Solution/src/com/greatlearning/departments/HrDept.java
+++ OOPsAssignment1Solution/src/com/greatlearning/departments/HrDept.java
@@ -3,23 +3,19 @@ package com.greatlearning.departments;
 public class HrDept extends SuperDept{
 	
 	public String departmentName() {
-		System.out.println("  Welcome to HR Department");
-		return " HR Department";
+		return " Welcome to HR Department";
 	}
 	
 	public String getTodaysWork() {
-		System.out.println(" Fill today’s worksheet and mark your attendance");
 		return " Fill today’s worksheet and mark your attendance";
 	}
 	
 	public String getWorkDeadline() {
-		System.out.println(" Complete by EOD");
 		return " Complete by EOD";
 	}
 	
 	public String doActivity() {
-		System.out.println(" team lunch");
-		return "team lunch";
+		return " team lunch";
 	}
 
 }
