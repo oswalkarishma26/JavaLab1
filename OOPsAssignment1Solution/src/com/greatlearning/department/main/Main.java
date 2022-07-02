28ccb820c98c72babb05cf76f4875c970049cdc5
oswalkarishma26 <107704168+oswalkarishma26@users.noreply.github.com>
2022-07-03 00:11:05 +0530

    Update Main.java

--- OOPsAssignment1Solution/src/com/greatlearning/department/main/Main.java
+++ OOPsAssignment1Solution/src/com/greatlearning/department/main/Main.java
@@ -14,26 +14,26 @@ public class Main {
 		HrDept hrDept = new HrDept();
 		TechDept techDept = new TechDept();
 		
-		adminDept.departmentName();
-		adminDept.getTodaysWork();
-		adminDept.getWorkDeadline();
-		superDept.isTodayHoliday();
+		System.out.println(adminDept.departmentName());
+		System.out.println(adminDept.getTodaysWork());
+		System.out.println(adminDept.getWorkDeadline());
+		System.out.println(superDept.isTodayHoliday());
 		
 		System.out.println();
 		
-		hrDept.departmentName();
-		hrDept.doActivity();
-		hrDept.getTodaysWork();
-		hrDept.getWorkDeadline();
-		superDept.isTodayHoliday();
+		System.out.println(hrDept.departmentName());
+		System.out.println(hrDept.doActivity());
+		System.out.println(hrDept.getTodaysWork());
+		System.out.println(hrDept.getWorkDeadline());
+		System.out.println(superDept.isTodayHoliday());
 		
 		System.out.println();
 		
-		techDept.departmentName();
-		techDept.getTodaysWork();
-		techDept.getWorkDeadline();
-		techDept.getTechStackInformation();
-		superDept.isTodayHoliday();
+		System.out.println(techDept.departmentName());
+		System.out.println(techDept.getTodaysWork());
+		System.out.println(techDept.getWorkDeadline());
+		System.out.println(techDept.getTechStackInformation());
+		System.out.println(superDept.isTodayHoliday());
 	}
 
 }
