b52a2a6eb9d2ee073f3508c47b80dfe9558693b5
oswalkarishma26 <107704168+oswalkarishma26@users.noreply.github.com>
2022-07-03 00:12:31 +0530

    Update AdminDept.java

--- OOPsAssignment1Solution/src/com/greatlearning/departments/AdminDept.java
+++ OOPsAssignment1Solution/src/com/greatlearning/departments/AdminDept.java
@@ -3,17 +3,14 @@ package com.greatlearning.departments;
 public class AdminDept extends SuperDept {
 	
 	public String departmentName() {
-		System.out.println("  Welcome to Admin Department");
-		return " Admin Department";
+		return "  Welcome to Admin Department";
 	}
 	
 	public String getTodaysWork() {
-		System.out.println(" Complete your documents submission");
-		return " Complete your documents Submission";
+		return " Complete your documents submission";
 	}
 	
 	public String getWorkDeadline() {
-		System.out.println(" Complete by EOD");
 		return " Complete by EOD";
 	}
 
